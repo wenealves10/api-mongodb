0949d06c32186e3043e898648fa3b45e1b40e914
Wene Alves de Oliveira <weneplay5@gmail.com>
2020-12-30 10:08:04 -0300

    session resgate

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,5 +1,9 @@
 const Users = require('../models/UsersModel');
 
 module.exports = (req, res) =>{
-   res.render('index');
+   // res.locals.name
+   // req.flash('info',`Olá ${req.session.user.name}`);
+   // console.log(req.session.user);
+   // console.log(req.flash('info'));
+   res.render('index',{users: req.session.user});
 }
